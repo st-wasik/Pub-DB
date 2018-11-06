86f97b851f639c4f4a0758b43b7d446b1be9e058
Stanisław Wasik <st.wasik@gmail.com>
2018-11-06 20:51:14 +0100

    createTables script refactor

--- PubDBApplication/PubDBApplication/App_Data/tables/createTables.sql
+++ PubDBApplication/PubDBApplication/App_Data/tables/createTables.sql
@@ -1,10 +1,10 @@
-﻿create table Adress
+﻿create table Address
 (
 id int primary key,
-nr int not null,
+building_no int not null,
 street varchar(30) not null,
 city varchar(30) not null,
-[post code] varchar(6)
+postal_code varchar(6)
 );
 GO;
 ------
@@ -12,15 +12,15 @@ create table Pubs
 (
 id int primary key,
 name varchar(30) not null,
-[adress id] int references Adress(id) not null
+adress_id int references Address(id) not null
 );
 GO;
 ------
-create table Producents
+create table Producers
 (
 id int primary key,
 name varchar(30) not null,
-[adress id] int references Adress(id) not null
+adress_id int references Address(id) not null
 );
 GO;
 ------
@@ -28,8 +28,10 @@ create table Products
 (
 id int primary key,
 name varchar(30) not null,
-[producent id] int references Producents(id) not null,
-price money not null
+producer_id int references Producers(id) not null,
+price money not null,
+alcohol_percentage int not null,
+volume real not null
 );
 GO;
 ------
@@ -43,29 +45,29 @@ GO;
 create table Orders
 (
 id int primary key,
-[magazine id] int references Magazines(id) not null,
-[pub id] int references Pubs(id),
-[producent id] int references Producents(id),
+magazine_id int references Magazines(id) not null,
+pub_id int references Pubs(id),
+producer_id int references Producents(id),
 [Incoming/Outcoming] int not null,
 status varchar(30) not null,
 [date] date not null
 );
 GO;
 ------
-create table [Order Details]
+create table OrderDetails
 (
 id int primary key,
-[order id] int references Orders(id) not null,
+order_id int references Orders(id) not null,
 quantity int not null,
-[product id] int references Products(id) not null
+product_id int references Products(id) not null
 );
 GO;
 ------
-create table [Magazines Stock]
+create table MagazinesStock
 (
 id int primary key,
-[magazine id] int references Magazines(id) not null,
-[product id] int references Products(id) not null,
+magazine_id int references Magazines(id) not null,
+product_id int references Products(id) not null,
 quantity int not null
 );
 GO;
