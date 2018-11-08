1bfbfc65f630b5c017108ac293f7d8c9f7090c5a
Qroshi <szymon1804@wp.pl>
2018-11-08 14:16:34 +0100

    zmiana alterów

--- PubDBApplication/PubDBApplication/App_Data/tables/alterTable.sql
+++ PubDBApplication/PubDBApplication/App_Data/tables/alterTable.sql
@@ -3,11 +3,13 @@ add check(building_no>0);
 
 alter table Pubs
 add unique(name),
-e-mail varchar(50) not null unique;
+e-mail varchar(50) unique,
+telephone_no varchar(12) unique;
 
 alter table Producers
 add unique(name),
-e-mail varchar(50) not null unique;
+e-mail varchar(50) unique,
+telephone_no varchar(12) unique;
 
 alter table Products
 add unique(name),
