15d3d7181b86018d1dd1d818cca3c2f294d3234b
Stanisław Wasik <st.wasik@gmail.com>
2018-11-08 14:45:13 +0100

    view script

--- PubDBApplication/PubDBApplication/App_Data/tables/views.sql
+++ PubDBApplication/PubDBApplication/App_Data/tables/views.sql
@@ -1,3 +1,3 @@
-create or replace view AddingToPubs
+create view AddingToPubs
 AS
-SELECT * FROM Pubs, Address;
+SELECT p.name, a.building_no, a.street, a.city, a.postal_code FROM Pubs p join Address a on p.adress_id=a.id;
