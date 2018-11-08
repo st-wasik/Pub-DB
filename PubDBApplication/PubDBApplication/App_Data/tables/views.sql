f786ce782b3277bf3b3df6ba54620a51ca6e7e8b
Qroshi <szymon1804@wp.pl>
2018-11-08 14:52:42 +0100

    trigger

--- PubDBApplication/PubDBApplication/App_Data/tables/views.sql
+++ PubDBApplication/PubDBApplication/App_Data/tables/views.sql
@@ -1,3 +1,12 @@
 create view AddingToPubs
 AS
 SELECT p.name, a.building_no, a.street, a.city, a.postal_code FROM Pubs p join Address a on p.adress_id=a.id;
+
+create or replace trigger trig1 on AddingToPubs
+instead of insert
+as
+begin
+insert into AddingToPubs
+select name,building_no, street, city, postal_code
+from inserted;
+end
