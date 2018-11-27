b83d5a30c6836ae0675802eec93f7593ebc84586
Qroshi <szymon1804@wp.pl>
2018-11-27 13:59:46 +0100

    Zmiany w triggerach

--- PubDBApplication/PubDBApplication/App_Data/tables/triggers.sql
+++ PubDBApplication/PubDBApplication/App_Data/tables/triggers.sql
@@ -1,16 +1,18 @@
-create or replace trigger trig1 on AddingToPubs
+create trigger InsertingToPubsView on PubsView
 instead of insert
 as
 begin
 insert into Address 
 select building_no, street, city, postal_code
 from inserted;
-insert into Pubs(name,adress_id) select name, (select a.id from Address a where a.street = inserted.street) from inserted;
+insert into Pubs(name,adress_id) select name, (select a.id from Address a where a.street = inserted.street and a.building_no = inserted.building_no and
+a.city = inserted.city and a.postal_code = inserted.postal_code) 
+from inserted;
 end
 
 create trigger Address_build_no
 ON Address
-INSTEAD OF insert
+AFTER insert
 AS
 BEGIN
 if((SELECT building_no FROM inserted)<1)
@@ -19,8 +21,20 @@ RAISERROR('NUMER BUDYNKU NIE MOZE BYC MNIEJSZY NIZ 1',16,1);
 rollback;
 return
 END
-insert into Address(building_no,street,city,postal_code) select building_no,street,city,postal_code from inserted;
 END
+
+create trigger UniqueAddress on Address
+after insert
+as
+begin
+if((SELECT COUNT(*) FROM Address a WHERE where a.street = inserted.street and a.building_no = inserted.building_no and
+a.city = inserted.city and a.postal_code = inserted.postal_code)>1)
+begin
+PRINT N'Address already exists in DataBase';
+rollback;
+return
+end
+end
 
 
 
