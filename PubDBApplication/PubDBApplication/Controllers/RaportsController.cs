e5e62d5b78af1addc3b4505cb6b5561461a457c8
Stanisław Wasik <st.wasik@gmail.com>
2019-01-03 14:40:20 +0100

    linq query

---
+++ PubDBApplication/PubDBApplication/Controllers/RaportsController.cs
@@ -0,0 +1,21 @@
+﻿using PubDBApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PubDBApplication.Controllers
+{
+    public class RaportsController : Controller
+    {
+        private PubDBEntities db = new PubDBEntities();
+
+        // GET: Raports/Details/5
+        public ActionResult Details(int id = 0)
+        {
+            ViewBag.RaportType = id;
+            return View();
+        }
+    }
+}
